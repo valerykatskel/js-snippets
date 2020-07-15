82385aca678e4762ec45cc4b17cfd4bccd2a1690
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-15 15:25:28 +0300

    3+1 free of one pruduct voucher added

--- cart-and-disconts.js
+++ cart-and-disconts.js
@@ -1,126 +1,231 @@
-class Discount {
+class Voucher {
   constructor() {
-    // We'll user the Singleton pattern in order to disable apply one code twice
-    if (Discount.exists) {
-      return Discount.instance;
+    if (Voucher.exists) {
+      return Voucher.instance;
     }
-    Discount.instance = this;
-    Discount.exists = true;
-    // The table of codes. Each code means the value of discont in percents. when we aply the code it will remove from the list
-    this.codes = [
-      ["aabb", 3],
-      ["babb", 3],
-      ["babe", 7],
-      ["aaab", 7],
-      ["bbbb", 12],
-      ["ddeb", 12],
-    ];
-    // This is the array of discounts by date. There are two discounts:
-    // 1. from, 14.07.2020 to 18.07.2020 - 5% (active)
-    // 2. from, 20.08.2020 to 22.08.2020 - 10% (active)
-    // If current date Date.now() is between the startTimestamp and endTimestamp then
-    // discountValue will apply automatically for total cart price
-    this.discounts = [
-      { start: 1594674000000, end: 1595019600000, value: 5, active: false }, // from, 14.07.2020 to 18.07.2020 5% of discount
-      { start: 1597870800000, end: 1598043600000, value: 10, active: true }, // from, 20.08.2020 to 22.08.2020 10% of discount
+    Voucher.instance = this;
+    Voucher.exists = true;
+
+    this.vouchers = [
+      {
+        start: 1594674000000, // 14.07.2020
+        end: 1595019600000, // 18.07.2020
+        type: "interval",
+        value: "",
+        discount: 10, // 10% of discount
+        active: false,
+      },
+      {
+        start: 1597870800000, // 20.08.2020
+        end: 1598043600000, // 22.08.2020
+        type: "interval",
+        value: "",
+        discount: 5, // 5% of discount
+        active: false,
+      },
+      {
+        start: 1594674000000, // 14.07.2020
+        end: 1598043600000, // 22.08.2020
+        type: "code",
+        value: "aabb",
+        discount: 3, // 3% of discount
+        active: true,
+      },
+      {
+        start: 1594674000000, // 14.07.2020
+        end: 1598043600000, // 22.08.2020
+        type: "code",
+        value: "bbbb",
+        discount: 4, // 4% of discount
+        active: true,
+      },
+      {
+        start: 1594674000000, // 14.07.2020
+        end: 1598043600000, // 22.08.2020
+        type: "bundle",
+        value: "3",
+        discount: 0, // 1 iteme will be for free if total count of items will be more than 3
+        active: true,
+      },
+      {
+        start: 1594674000000, // 14.07.2020
+        end: 1598043600000, // 22.08.2020
+        type: "bulk",
+        value: "80",
+        discount: 4, // 4% of discount
+        active: true,
+      },
     ];
   }
 
-  applyCode(c) {
-    const [, val] = this.codes.filter((code) => code[0] == c).flat();
+  applyVoucher(code) {
+    let discountValue = 0;
+    discountValue += this.checkIntervalDiscount();
 
-    if (val != undefined) {
-      this.codes = this.codes.filter((code) => code[0] != c);
-      return val;
-    } else return 0;
+    if (code != "") {
+      discountValue += this.checkCodeDiscount(code);
+    }
+
+    //this.checkBulkOrderDiscount();
+    return discountValue;
   }
 
-  applyByDate() {
-    const discount = this.discounts.filter((discount) => {
-      const now = Date.now();
-      return now >= discount.start && now <= discount.end;
-    });
-    let res = 0;
-    if (discount.length > 0) {
-      const [d] = discount;
-      const { value, active, ...rest } = d;
-      res = value > 0 && active ? value : 0;
-    } else res = 0;
-    return res;
+  getVoucherByType(type) {
+    const now = Date.now();
+    return this.vouchers.filter(
+      (v) => v.active && now >= v.start && now <= v.end && v.type == type
+    );
+  }
+
+  checkIntervalDiscount() {
+    const d = this.getVoucherByType("interval");
+    if (d.length === 0) return 0;
+    const [discount] = d;
+    return discount.discount;
+  }
+
+  checkBundleDiscount() {
+    const d = this.getVoucherByType("bundle");
+    if (d.length === 0) return 0;
+    const [discount] = d;
+    const { value: bundleSize, discount: discountSize, ...rest } = discount;
+    return [bundleSize, discountSize];
+  }
+
+  checkCodeDiscount(code) {
+    const d = this.getVoucherByType("code").filter((v) => v.value === code);
+
+    if (d.length === 0) return 0;
+    const [discount] = d;
+    const { value: discountCode, discount: discountValue, ...rest } = discount;
+
+    // remove used voucher
+    // this.vouchers = this.vouchers.filter(
+    //   (v) => v.type == "code" && v.value != code
+    // );
+    return discountValue;
+  }
+
+  checkBulkOrderDiscount() {
+    const bulkDiscount = this.getVoucherByType("bulk");
+    if (bulkDiscount.length === 0) return 0;
+    const [discount] = bulkDiscount;
+    const { value: bulkSize, discount: discountValue, ...rest } = discount;
   }
 }
 
 class Cart {
-  constructor() {
+  constructor(name = "user cart", freeShipping = 100) {
+    this.name = name;
     this.items = [];
     this.discount = 0;
+    this.freeShipping = freeShipping;
+    this.shippingPrice = 14;
+    this.userDiscountCode = "";
   }
 
-  getDiscount() {
-    return this.discount;
-  }
-  applyByDate() {
-    const discount = new Discount();
-    const discountValue = discount.applyByDate();
-    this.discount = discountValue ? discountValue : 0;
+  applyDiscountCode(code) {
+    if (code === undefined || code == "") return;
+    this.userDiscountCode = code;
   }
 
   getTotalPrice() {
-    this.applyByDate();
-    let total = this.items.reduce((total, el) => total + el.price, 0);
-    return this.discount > 0 ? total - total * (this.discount / 100) : total;
-  }
+    let totalPrice = this.items.reduce(
+      (total, el) => total + el.price * el.count,
+      0
+    );
 
-  applyCode(code) {
-    const discount = new Discount();
-    const discountValue = discount.applyCode(code); // If discount by date has been applied, we don't apply discount by code
+    let totalCount = this.items.reduce((total, el) => total + el.count, 0);
 
-    if (discountValue > this.discount) {
-      this.discount = discountValue;
+    const voucher = new Voucher();
+    this.discount = voucher.applyVoucher(this.userDiscountCode);
+    const [bundleSize] = voucher.checkBundleDiscount();
+
+    totalPrice =
+      this.discount > 0
+        ? totalPrice - totalPrice * (this.discount / 100)
+        : totalPrice;
+
+    if (totalCount > bundleSize) {
+      const [minPriceItem, ...rest] = this.items.sort(
+        (a, b) => a.price - b.price
+      );
+      const { price: minPrice, ...r } = minPriceItem;
+      totalPrice -= minPrice;
     }
+
+    return totalPrice;
   }
 
-  add(product) {
+  getTotalCount() {
+    return this.items.reduce((total, el) => total + el.count, 0);
+  }
+
+  getShippingPrice(totalPrice) {
+    return totalPrice >= this.freeShipping ? 0 : this.shippingPrice;
+  }
+
+  addItem(product) {
     return this.items.push(product);
   }
 
-  show() {
-    let res = "Cart includes:\n";
-    this.items.forEach(
-      (el, id) =>
-        (res += `#${id + 1}\t${el.name} (${el.color})\t\$${el.price}\n`)
-    );
-
-    res += `===================================\nTotal:\t\t\t\t\$${this.getTotalPrice()}${
-      this.discount ? ` (-${this.discount}%)\n` : "\n"
-    }`;
+  renderCartItems() {
+    let res = "";
+    this.items.forEach((el) => (res += this.renderCartItem(el)));
     return res;
   }
 
-  applyVoucher() {}
+  renderCartItem({ name, color, price, count }) {
+    return `${name} (${color})\t\$${price}\tx${count} = ${price * count}\n`;
+  }
+
+  renderCartFooter() {
+    let res = "";
+    const totalPrice = this.getTotalPrice();
+    const shippingPrice = this.getShippingPrice(totalPrice);
+    const totalCount = this.getTotalCount();
+
+    // Render total count
+    res += "========================================\n";
+    res += `Total count:\t\t\t${totalCount} pcs\n`;
+
+    // Render total price
+    res += `Total price:\t\t\t\$${totalPrice}${
+      this.discount ? ` (-${this.discount}%)\n` : "\n"
+    }`;
+
+    // Render shipping price
+    res += `Shipping:\t\t\t\$${shippingPrice}`;
+
+    return res;
+  }
+
+  renderCart() {
+    const items = this.renderCartItems();
+    const footer = this.renderCartFooter();
+    return `${this.name}:\n${items}${footer}`;
+  }
 }
 
-const cart = new Cart();
+const cart = new Cart("Cart #1");
 // add some products to cart
-cart.add({ name: "Product 1", color: "Blue", price: 10 });
-cart.add({ name: "Product 2", color: "Green", price: 15 });
-cart.add({ name: "Product 3", color: "White", price: 5 });
-cart.add({ name: "Product 4", color: "Yellow", price: 19 });
+cart.addItem({ name: "Product 1", color: "Blue", price: 10, count: 1 });
+cart.addItem({ name: "Product 2", color: "Green", price: 15, count: 1 });
+cart.addItem({ name: "Product 3", color: "White", price: 5, count: 1 });
+cart.addItem({ name: "Product 4", color: "Yellow", price: 1, count: 1 });
 
-// show the cart before discount was applied
-console.log(`!!!Before discount was applied!!!\n${cart.show()}`);
+// apply discount voucher by code
+//cart.applyDiscountCode("aabb");
 
-// apply discount voucher
-//cart.applyCode("aabb");
+// show the cart after discount code was applied
+console.log(`\n\n\n${cart.renderCart()}`);
 
-// show the cart after discount was applied
-//console.log(cart.show());
+const cart2 = new Cart("Cart #2");
+cart2.addItem({ name: "Product 1", color: "White", price: 6, count: 10 });
+cart2.addItem({ name: "Product 2", color: "Blue", price: 8, count: 2 });
+cart2.addItem({ name: "Product 3", color: "Black", price: 11, count: 3 });
+cart2.addItem({ name: "Product 4", color: "Yellow", price: 3, count: 4 });
 
-// const cart2 = new Cart();
-// cart2.add({ name: "Product 1", color: "Blue", price: 10 });
-// cart2.add({ name: "Product 2", color: "Green", price: 15 });
-// cart2.add({ name: "Product 3", color: "White", price: 8 });
-// cart2.add({ name: "Product 4", color: "Yellow", price: 19 });
-
-// cart2.applyCode("bbbb");
-// console.log(cart2.show());
+// apply discount voucher by code
+cart2.applyDiscountCode("bbbb");
+console.log(`\n\n\n${cart2.renderCart()}`);
